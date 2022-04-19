7cf16f0bf5c29e0e3ef722d986f479956240efe3
Dmitriy <khaitdmitriy@gmail.com>
2022-04-19 16:31:54 -0400

    added questions

--- src/components/MultipleChoice.js
+++ src/components/MultipleChoice.js
@@ -1,18 +1,45 @@
 import React from "react";
 
-function MultipleChoice(){
+function MultipleChoice({answers, correct}){
+  
+  //shuffle array function  
+  function shuffle(arr) {
+    let newArr = []
+    let randIndex 
+    let working = true
+      if (!arr) {
+          return
+      }
+      while (working){   
+        randIndex = (Math.floor(Math.random()*25)%arr.length)     
+        if (!newArr.includes(arr[randIndex])){
+          newArr.push(arr[randIndex])
+        }
+        if(newArr.length === arr.length){
+          working = false
+        }
+      }
+      return newArr
+    }
+
+    //check answer function
+    function checkAnswer(e){
+        console.log(e)
+    }
+
 
     return (
-        <div>
-        <h1>Answers</h1>
-        <div> 
-        <p> </p>
-        <p> </p>
-        <p> </p>
-        <p> </p>
-        </div>
-        </div>
-    )
+       <div>
+        {
+            shuffle(answers).map((e)=> {
+                return <button class="btn" style={{ background: 'lightgreen' }} onClick={(e) => checkAnswer(e)}> {e} </button>
+            }) 
+        }
+     </div>
+      
+      
+    );
+
 }
 
  export default MultipleChoice;
