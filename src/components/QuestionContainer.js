20176a9c6c19bd135285a2a940dea99fcfa12c10
Ilya <milyavskyfamily@gmail.com>
2022-04-20 17:31:23 -0400

    saves

--- src/components/QuestionContainer.js
+++ src/components/QuestionContainer.js
@@ -3,18 +3,26 @@ import React from "react";
 import Question from './Question'
 import MultipleChoice from "./MultipleChoice";
 
-function QuestionContainer({questions, points, setPoints}){
-    
+
+
+function QuestionContainer({questions}){
+
 return(
     <div>
-    <h1>Trivia</h1> 
-    {
-        questions.map((element) => {
-            return <Question question={element} points={points} setPoints={setPoints} /> 
-        })
-    }    
-    </div>   
+    <h1>Trivia</h1>
+   
+    
+    {questions}.map((e)={
+    <Question data= {e}/>
+
+})
+    <MultipleChoice />
+  
+    </div>
+    
 )
+
+
 }
 
 export default QuestionContainer;
---
+++ src/components/Startpage.js
@@ -0,0 +1,17 @@
+import React, {useState} from 'react';
+
+function Startpage(){
+
+    <span>
+    <h1 className="welcome-header">
+    WELCOME TO TRIVIA!
+    </h1>
+    </span>
+
+
+
+
+
+}
+
+export default Startpage;
