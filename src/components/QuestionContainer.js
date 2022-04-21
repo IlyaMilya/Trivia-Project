e1ba70db1811ce0cde1769b8812df055fbe0d76a
Ilya <milyavskyfamily@gmail.com>
2022-04-21 18:24:53 -0400

    adding main branch to mine

--- src/components/QuestionContainer.js
+++ src/components/QuestionContainer.js
@@ -3,26 +3,19 @@ import React from "react";
 import Question from './Question'
 import MultipleChoice from "./MultipleChoice";
 
-
-
-function QuestionContainer({questions}){
-
+function QuestionContainer({questions, points, setPoints}){
+    
 return(
     <div>
-    <h1>Trivia</h1>
-   
+    {
+        questions.map((element) => {
+            return <Question question={element} points={points} setPoints={setPoints} /> 
+        })
+     
+    }    
     
-    {questions}.map((e)={
-    <Question data= {e}/>
-
-})
-    <MultipleChoice />
-  
-    </div>
-    
+    </div>   
 )
-
-
 }
 
 export default QuestionContainer;
